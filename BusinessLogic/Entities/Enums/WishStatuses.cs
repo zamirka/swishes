df8ec11638a9899b90c35b5ff218ca1834e5978f
Zamir Musaev <zamir.ru@gmail.com>
2013-11-19 00:32:28 +0400

    Пока не самые корректные миграции созданы

---
+++ BusinessLogic/Entities/Enums/WishStatuses.cs
@@ -0,0 +1,10 @@
+﻿namespace swishes.BusinessLogic.Entities.Enums
+{
+    public enum WishStatuses
+    {
+        FreeForPresent = 0,
+        Presented = 1,
+        BougthMyself = 2,
+        DontNeed =3
+    }
+}
---
+++ BusinessLogic/Entities/Profile/UserInRole.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace swishes.BusinessLogic.Entities.Profile
+{
+    [Table("webpages_UsersInRoles")]
+    public class UserInRole
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int UserId { get; set; }
+        
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int RoleId { get; set; }
+
+        public virtual ICollection<Role> Roles { get; set; }
+    }
+}
