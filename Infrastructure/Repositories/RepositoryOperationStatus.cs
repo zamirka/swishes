0870f18c0ee5e82578e2155cbb87e6bdabc59140
Zamir Musaev <zamir.ru@gmail.com>
2013-11-20 23:58:12 +0400

    DataContext and Generic Repository

---
+++ Infrastructure/Repositories/RepositoryOperationStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace swishes.Infrastructure.Repositories
+{
+    public enum RepositoryOperationStatus
+    {
+        Success = 0,
+        UnknownError = 1,
+        EntityAlreadyExists = 2,
+        EntityNotFound = 3,
+        NotAllEntitiesProcessed = 4
+    }
+}
